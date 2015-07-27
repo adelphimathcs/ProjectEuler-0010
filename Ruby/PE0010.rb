ea5ae16adca28eddbe70c9a6cedd33ce9ddcbb44
Michael Vessia <michaelvessia@mail.adelphi.edu>
2015-07-27 14:36:47 -0400

    replaced for loop with iterator

--- Ruby/PE0010.rb
+++ Ruby/PE0010.rb
@@ -1,10 +1,10 @@
 #Sieve of Eratosthenes for generating primes
-def sieve(up_to)
-  nums = (0..up_to).to_a
+def sieve(max)
+  nums = (0..max).to_a
   nums[0] = nums[1] = nil #2 is our first prime
-  for i in 2 .. Math.sqrt(up_to) #Only need to check up to sqrt
-    next if !nums[i] 
-    (i**2).step(up_to, i) do |j| #step by multiples of curent number
+  2.upto(Math.sqrt(max)) do |i| #Only need to check up to sqrt
+    next unless nums[i] 
+    (i**2..max).step(i) do |j| #step by multiples of curent number
       nums[j] = nil # mark
     end
   end
